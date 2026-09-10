9f6a29fd12c9555128eda996b57c404cf6640377
Hugo Gabriel <hugogabriel@rede.ulbra.br>
2026-09-10 14:37:34 -0300

    first commit

---
+++ ColetorLogsServidor/src/main/java/com/mycompany/coletorlogsservidor/ColetorLogsServidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coletorlogsservidor;
+
+/**
+ *
+ * @author 07872123185
+ */
+public class ColetorLogsServidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
