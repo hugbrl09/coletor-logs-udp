7cbfefa0475ae050d21ea118e2852c939a121c3f
Hugo Gabriel <hugogabriel@rede.ulbra.br>
2026-09-10 14:54:09 -0300

    feat: criação dos arquivos das classes

--- ColetorLogsCliente/src/main/java/com/mycompany/coletorlogscliente/Resposta.java
+++ ColetorLogsCliente/src/main/java/com/mycompany/coletorlogscliente/Resposta.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.coletorlogscliente;
 
 /**
  *
  * @author 07872123185
  */
-public class ColetorLogsCliente {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Resposta {
+    
 }
